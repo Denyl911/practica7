2835fa9487095f59b685ad88622b24341d599791
Denyl911 <imdenyl@gmail.com>
2023-02-26 21:52:51 -0600

    first commit

---
+++ src/main/java/com/mycompany/practica7/Practica7.java
@@ -0,0 +1,19 @@
+package com.mycompany.practica7;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.TreeSet;
+/**
+ *
+ * @author denyl
+ */
+public class Practica7 {
+    
+    
+
+    public static void main(String[] args) {
+        new ventana().setVisible(true);
+    }
+}
